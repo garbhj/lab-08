993596c659a2aeff3c16ddc0a2ed5d30809cf639
Jason Hu <garbhj6@gmail.com>
2026-03-03 18:28:55 -0700

    test: Created tests for deleteCity and countCities

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -13,4 +13,26 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testCountCity() {
+        CustomList list = new CustomList();
+        assertTrue(list.countCities() == 0);
+        list.addCity(new City("Calgary", "AB"));
+        assertTrue(list.countCities() == 1);
+        list.addCity(new City("Edmonton", "AB"));
+        assertTrue(list.countCities() == 2);
+    }
+
+
 }
